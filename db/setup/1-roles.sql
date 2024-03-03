b8a15380c6f2bf45b2cfc7cf2d3c422d98ef7e36
André Willyan <andrewillyan@tutanota.com>
2024-03-03 18:31:30 -0300

    setup do docker com o banco

---
+++ db/setup/1-roles.sql
@@ -0,0 +1,7 @@
+CREATE ROLE petiano_user LOGIN;
+
+CREATE ROLE aluno_user LOGIN;
+
+CREATE ROLE escola_user LOGIN;
+
+CREATE ROLE api_user LOGIN;
--- db/setup_oci_interface.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE IF NOT EXISTS oci_interface;
-USE oci_interface;
